133b5319c69e80a4f9a58471878591a31d5cd02d
Aimun Aziz <aimunaziz04@gmail.com>
2022-09-16 02:40:03 -0700

    Project in new branch

--- connection/connection.js
+++ connection/connection.js
@@ -4,7 +4,8 @@ const connection = mysql.createConnection({
     host : 'mysql-88296-0.cloudclusters.net',
     user : 'admin',
     password : 'hbkYKbjH',
-    database : 'banking'
+    database : 'banking',
+    port : 16651
 });
 connection.connect()
 
